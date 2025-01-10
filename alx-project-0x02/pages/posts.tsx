eddca14a8b2e32783f82c07c712d335d43d5c50d
WorkuUrga <urgaworku@gmail.com>
2025-01-10 15:35:49 +0300

    corrections

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,12 +1,31 @@
 import Header from '@/components/layout/Header';
+import PostCard from '@/components/common/PostCard'; // Ensure you have this component
+import { PostProps } from '@/interfaces'; // Make sure PostProps interface is defined
 
-const PostsPage = () => {
+const PostsPage = ({ posts }: { posts: PostProps[] }) => {
   return (
     <div>
       <Header />
       <h1>Posts Page</h1>
+      <div>
+        {posts.map((post) => (
+          <PostCard key={post.id} title={post.title} content={post.body} userId={post.userId} />
+        ))}
+      </div>
     </div>
   );
+};
+
+// Fetch data using getStaticProps
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts: PostProps[] = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default PostsPage;
